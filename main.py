63345b6b7d2f87aca703797c6d1c9b11d6df65af
yhlas <wk.styhlas21@gmail.com>
2024-11-29 20:57:05 +0300

    with refreshed gitignore

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import psycopg2             # python->psql connection
 import psycopg2.extras
-
-#import pandas as pd         # create dataframes 
-
 import os                   # fetch files
-
 import time                 # timing operations
 import memory_profiler      # managing memory usage
 from memory_profiler import memory_usage
 
 from functools import wraps # decorator/wrapper
-
 from typing import Iterator, Optional, Dict, Any,List  # Create Iterator for One-By-One Loading 
-
 import io
 
 # Import the 'config' function from the config_user_dta.py file:
